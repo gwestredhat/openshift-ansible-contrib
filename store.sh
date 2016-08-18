9a349cf200d2e232fe8eb3f053cf689274e12738
Glenn West <glennswest@yahoo.com.sg>
2016-08-18 12:06:09 +0800

    First addition of a iscsi storage array vm

---
+++ store.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#yum -y update
+yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash-completion 
+
+                                                                                         
+
