840adc67a087899ed711cd8858c96ce2040202e5
Glenn West <glennswest@yahoo.com.sg>
2016-10-11 09:34:34 +0800

    Remove the uneeded retry loop

--- bastion.sh
+++ bastion.sh
@@ -252,13 +252,8 @@ EOF
 cat <<EOF > /home/${AUSERNAME}/openshift-install.sh
 export ANSIBLE_HOST_KEY_CHECKING=False
 sleep 120
-ansible all --module-name=ping > ansible-preinstall-ping.out
-RET=1
-until [ \${RET} -eq 0 ]; do
-   ansible-playbook -vvv /home/${AUSERNAME}/subscribe.yml
-   RET=\$?
-   sleep 10
-done
+ansible all --module-name=ping > ansible-preinstall-ping.out || true
+ansible-playbook -vvv /home/${AUSERNAME}/subscribe.yml
 echo "${RESOURCEGROUP} Bastion Host is starting ansible BYO" | mail -s "${RESOURCEGROUP} Bastion BYO Install" ${RHNUSERNAME} || true
 ansible-playbook  /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml < /dev/null &> byo.out
 # ssh gwest@master1 oadm registry --selector=region=infra
